1bc4b5ae5aaeebd1e0694dae47b93f583334953c
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-01 14:04:45 -0400

    add property

---
+++ src/app/core/property/property.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prb-property',
+  imports: [],
+  template: `
+    <ng-content></ng-content>
+  `,
+  styleUrl: './property.scss',
+})
+export class Property {}
