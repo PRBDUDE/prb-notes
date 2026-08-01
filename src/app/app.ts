8bd84851b4f6f5d51874967cd371ca4df8c6fdec
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-01 13:55:44 -0400

    add imports to app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '@core/header';
+import { Footer } from '@core/footer';
+import { Menu } from '@core/menu';
 
 @Component({
   selector: 'prb-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Menu],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('prb-notes');
