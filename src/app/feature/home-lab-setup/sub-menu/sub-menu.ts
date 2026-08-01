f04e04e1bf13be27feb1ed92c682c0cb271fe63f
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-01 14:06:06 -0400

    add sub-menus

---
+++ src/app/feature/home-lab-setup/sub-menu/sub-menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prb-sub-menu',
+  imports: [],
+  templateUrl: './sub-menu.html',
+  styleUrl: './sub-menu.scss',
+})
+export class SubMenu {}
