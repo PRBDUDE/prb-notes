9c17edd27464eb0a03bc7fae4a19137a10e1d304
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-13 11:43:48 -0400

    move micro-front-end-sub-menu

--- src/app/feature/micro-front-ends/micro-front-end-sub-menu/micro-front-end-sub-menu.ts
+++ src/app/feature/micro-front-ends/micro-front-end-sub-menu/micro-front-end-sub-menu.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'prb-micro-front-end-sub-menu',
+  selector: 'prb-micro-front-end-micro-front-end-sub-menu',
   imports: [],
   templateUrl: './micro-front-end-sub-menu.html',
   styleUrl: './micro-front-end-sub-menu.scss',
