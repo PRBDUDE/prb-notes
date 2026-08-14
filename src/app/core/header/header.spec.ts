dc7612f947984b17a3751e3aa3716cb4ca1178a9
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-14 13:19:18 -0400

    correct header.spec

--- src/app/core/header/header.spec.ts
+++ src/app/core/header/header.spec.ts
@@ -1,22 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Header } from './header';
+import { HeaderService } from '@core/header-service';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { MockHeaderService } from '@mock/mock-header-service';
 
-describe('Header', () => {
+describe('Header Component', () => {
   let component: Header;
   let fixture: ComponentFixture<Header>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Header],
+      providers: [{ provide: HeaderService, useClass: MockHeaderService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Header);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should inject HeaderService correctly', () => {
+    expect(component.headerService).toBeDefined();
+    expect(component.headerService).toBeInstanceOf(MockHeaderService);
   });
 });
