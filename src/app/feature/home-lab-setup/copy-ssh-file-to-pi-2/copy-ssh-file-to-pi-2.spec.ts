39e02d7109f9f8e9d01d04413d126b6f65d38035
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-15 10:26:31 -0400

    add tests

--- src/app/feature/home-lab-setup/copy-ssh-file-to-pi-2/copy-ssh-file-to-pi-2.spec.ts
+++ src/app/feature/home-lab-setup/copy-ssh-file-to-pi-2/copy-ssh-file-to-pi-2.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { describe, beforeEach, it, expect } from 'vitest';
 import { CopySshFileToPi2 } from './copy-ssh-file-to-pi-2';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CopySshFileToPi2', () => {
   let component: CopySshFileToPi2;
@@ -8,15 +10,25 @@ describe('CopySshFileToPi2', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CopySshFileToPi2],
+      imports: [
+        CopySshFileToPi2
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CopySshFileToPi2);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the correct page subtitle', () => {
+    expect(component.pageSubtitle).toBe('Home Lab Setup - Copy SSH File to PI (2)');
   });
 });
