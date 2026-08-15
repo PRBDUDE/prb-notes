07606142b6626b0ee84ecd0417d22b8ace187bbc
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-15 10:20:06 -0400

    add tests

--- src/app/feature/home-lab-setup/copy-ssh-file-to-pi/copy-ssh-file-to-pi.spec.ts
+++ src/app/feature/home-lab-setup/copy-ssh-file-to-pi/copy-ssh-file-to-pi.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CopySshFileToPi } from './copy-ssh-file-to-pi';
+import { describe, beforeEach, it, expect } from 'vitest';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CopySshFileToPi', () => {
   let component: CopySshFileToPi;
@@ -8,15 +10,25 @@ describe('CopySshFileToPi', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CopySshFileToPi],
+      imports: [
+        CopySshFileToPi
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CopySshFileToPi);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the correct pageSubtitle', () => {
+    expect(component.pageSubtitle).toBe('Home Lab Setup - Copy SSH File to PI');
   });
 });
--- src/app/feature/home/home.spec.ts
+++ src/app/feature/home/home.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Home } from './home';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-describe('Home', () => {
+describe('Home Component', () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home],
+      imports: [
+        Home
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the correct pageSubtitle', () => {
+    expect(component.pageSubtitle).toBe('Home');
   });
 });
