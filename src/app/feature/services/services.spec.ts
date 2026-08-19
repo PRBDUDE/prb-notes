24fd2caeb9482ce81948acf4f8ad8452d78a4cca
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-19 08:59:33 -0400

    add tests

--- src/app/feature/services/services.spec.ts
+++ src/app/feature/services/services.spec.ts
@@ -1,22 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Services } from './services';
+import { ServicesSubMenu } from './services-sub-menu/services-sub-menu';
+import { MockServicesSubMenu } from '@mock/mock-services-sub-menu';
 
-describe('Services', () => {
+describe('Services Component', () => {
   let component: Services;
   let fixture: ComponentFixture<Services>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Services],
-    }).compileComponents();
+    })
+      .overrideComponent(Services, {
+        remove: { imports: [ServicesSubMenu] },
+        add: { imports: [MockServicesSubMenu] },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(Services);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set pageSubtitle to "Services"', () => {
+    expect(component.pageSubtitle).toBe('Services');
   });
 });
