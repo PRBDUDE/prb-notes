d1991640e63f4fda780d30a30c0e7021022666ca
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-21 15:23:15 -0400

    add type

--- src/app/core/indent/indent.ts
+++ src/app/core/indent/indent.ts
@@ -1,5 +1,7 @@
 import { Component, input } from '@angular/core';
 
+export type IndentLevelType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
 @Component({
   selector: 'prb-indent',
   imports: [],
@@ -11,5 +13,5 @@ import { Component, input } from '@angular/core';
   styleUrl: './indent.scss',
 })
 export class Indent {
-  level = input<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9>(1);
+  level = input<IndentLevelType>(1);
 }
