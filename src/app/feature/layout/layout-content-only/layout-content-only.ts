fb699abd39270874dfd54a775e56a5eb14c8a06b
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-21 15:28:13 -0400

    add layout-content

---
+++ src/app/feature/layout/layout-content-only/layout-content-only.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prb-layout-content-only',
+  imports: [],
+  templateUrl: './layout-content-only.html',
+  styleUrls: ['../layout.scss', './layout-content-only.scss'],
+})
+export class LayoutContentOnly {}
