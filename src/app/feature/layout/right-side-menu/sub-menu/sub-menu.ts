8350291926d2ed01dbba1822e3e7466c8ac3a4b2
PRBDUDE <programmerroadblocks@gmail.com>
2026-08-21 15:25:43 -0400

    add left-side/sub-menu add no-side/sub-menu add right-side/sub-menu

---
+++ src/app/feature/layout/right-side-menu/sub-menu/sub-menu.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prb-sub-menu',
+  imports: [],
+  templateUrl: './sub-menu.html',
+  styleUrl: './sub-menu.scss',
+  host: {
+    class: 'right-menu',
+  },
+})
+export class SubMenu {}
