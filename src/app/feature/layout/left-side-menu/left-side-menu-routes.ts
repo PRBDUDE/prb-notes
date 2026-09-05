438df48e47928fdea2de17c2027cd9322574cce1
PRBDUDE <programmerroadblocks@gmail.com>
2026-09-04 21:20:59 -0400

    remove grid-layout route

--- src/app/feature/layout/left-side-menu/left-side-menu-routes.ts
+++ src/app/feature/layout/left-side-menu/left-side-menu-routes.ts
@@ -5,10 +5,5 @@ export const left_side_menu_routes: Routes = [
     path: '',
     loadComponent: () => import('./left-side-menu').then(m => m.LeftSideMenu),
     title: 'Left Side Menu',
-  },
-  {
-    path: 'grid-layout',
-    loadComponent: () => import('./grid-layout/grid-layout').then(m => m.GridLayout),
-    title: 'Grid Layout',
   }
 ]
